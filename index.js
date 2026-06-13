41513ae3ecc65f30909ffbf39eca7f5695aa9e7c
VADIVEL007-max <vadivels759@gmail.com>
2026-06-14 01:12:27 +0530

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express=require("express")
 const{ PrismaClient }=require("@prisma/client")
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app= express();
 const prisma = new PrismaClient();
+
+app.use(
+  cors({
+    origin: "https://react-ecommerce-nu-rouge.vercel.app"
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
